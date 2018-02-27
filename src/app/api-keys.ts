1b509e9de9ba6cc4da9f16b42be97259ad562717
Jun Li <june11084@gmail.com>
2018-02-26 22:00:54 -0800

    add firebase

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export var masterFirebaseConfig = {
+   apiKey: "AIzaSyAo6P5LMmcsnyFaC9gf_0-HE15Rfyv8xjU",
+   authDomain: "online-store-4e01f.firebaseapp.com",
+   databaseURL: "https://online-store-4e01f.firebaseio.com",
+   projectId: "online-store-4e01f",
+   storageBucket: "",
+   messagingSenderId: "431625522362"
+  };
